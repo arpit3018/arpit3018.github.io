9f989e2244657bfb7378c4df403cc9ffd2c6ead4
Arpit Agrawal <arpitagrawal3018@gmail.com>
2019-08-26 11:54:38 +0530

    blank

--- Content/site.js
+++
@@ -1,6 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register('service-worker.js')
-        .then(r => console.log("SW Registered"))
-        .catch(console.error);
-}
